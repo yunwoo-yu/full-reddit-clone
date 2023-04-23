94b0bbec005857e13328dbf9ef685208f0b6d89d
Ycow <skypnal12@gmail.com>
2023-04-23 22:23:41 +0900

    feat: 회원가입 페이지 작성 Input 컴포넌트 생성

---
+++ client/src/components/InputGroup.tsx
@@ -0,0 +1,40 @@
+import cls from 'classnames';
+
+interface Props {
+  className?: string;
+  type?: string;
+  placeholder?: string;
+  value: string;
+  error: string | undefined;
+  setValue: (str: string) => void;
+}
+
+function InputGroup({
+  className = 'mb-2',
+  type = 'text',
+  placeholder = '',
+  error,
+  value,
+  setValue,
+}: Props) {
+  return (
+    <div className={className}>
+      <input
+        type={type}
+        value={value}
+        style={{ minHeight: 300 }}
+        className={cls(
+          `w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white`,
+          {
+            'border-red-500': error,
+          }
+        )}
+        placeholder={placeholder}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <small className="font-medium text-red-500">{error}</small>
+    </div>
+  );
+}
+
+export default InputGroup;
