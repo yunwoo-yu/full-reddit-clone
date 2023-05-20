4f4920c780acf8d913c30b3c289bf804cbab3601
Ycow <skypnal12@gmail.com>
2023-05-20 19:40:28 +0900

    feat: 로그인, 회원가입 페이지 navBar 제거

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,13 +2,20 @@ import axios from 'axios';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import AuthContextProvider from '../context/auth';
+import { useRouter } from 'next/router';
+import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+  const authRoutes = ['/register', '/login'];
+  const authRoute = authRoutes.includes(pathname);
+
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
   axios.defaults.withCredentials = true;
 
   return (
     <AuthContextProvider>
+      {!authRoute && <Navbar />}
       <Component {...pageProps} />
     </AuthContextProvider>
   );
