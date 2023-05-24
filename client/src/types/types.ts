f66823af9cc6f54612b5a8b3b2eba832244d7501
Ycow <skypnal12@gmail.com>
2023-05-24 23:22:02 +0900

    feat: 타입 선언

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -4,3 +4,46 @@ export interface User {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface Post {
+  identifier: string;
+  title: string;
+  slug: string;
+  body: string;
+  subName: string;
+  username: string;
+  createdAt: string;
+  updatedAt: string;
+  sub?: Sub;
+  url: string;
+  userVote?: number;
+  voteScore?: number;
+  commentCount: number;
+}
+
+export interface Sub {
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  title: string;
+  description: string;
+  imageUrn: string;
+  bannerUrn: string;
+  username: string;
+  posts: Post[];
+  postCount?: string;
+  imageUrl: string;
+  bannerUrl: string;
+}
+
+export interface Comment {
+  identifier: string;
+  body: string;
+  username: string;
+  createdAt: string;
+  updatedAt: string;
+  post?: Post;
+
+  userVote: number;
+  voteScore: number;
+}
