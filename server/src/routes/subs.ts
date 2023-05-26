3b86e9542d9679ca31511644b4670a59922c46c5
Ycow <skypnal12@gmail.com>
2023-05-26 22:11:14 +0900

    feat: subs api 완성, 기본 이미지 url next.config 추가

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -5,6 +5,7 @@ import { isEmpty } from "class-validator";
 import { AppDataSource } from "../data-source";
 import Sub from "../entities/Subs";
 import { User } from "../entities/User";
+import Post from "../entities/Post";
 
 const createSub = async (req: Request, res: Response, next) => {
   const { name, title, description } = req.body;
@@ -41,13 +42,32 @@ const createSub = async (req: Request, res: Response, next) => {
     sub.user = user;
 
     await sub.save();
+
     return res.json(sub);
   } catch (error) {
     return res.status(500).json({ error: "문제가 발생했습니다." });
   }
 };
 
-const topSubs = (req: Request, res: Response) => {};
+const topSubs = async (req: Request, res: Response) => {
+  try {
+    const imageUrlExp = `COALESCE(s."imageUrn", 'https:///www.gravatar.com/avatar?d=mp&f=y')`;
+    const subs = await AppDataSource.createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
+      )
+      .from(Sub, "s")
+      .leftJoin(Post, "p", `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, "DESC")
+      .limit(5)
+      .execute();
+
+    return res.json(subs);
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+};
 
 const subRoutes = Router();
 
