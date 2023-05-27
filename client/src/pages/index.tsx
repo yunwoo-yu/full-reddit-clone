b9d434d2cafa7148019afb097355727f6d7e84bf
Ycow <skypnal12@gmail.com>
2023-05-28 02:12:33 +0900

    feat: index 페이지 완료, 상위커뮤니티 aside 추가

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,15 +3,16 @@ import Link from 'next/link';
 import useSWR from 'swr';
 import { Sub } from '../types/types';
 import axios from 'axios';
+import Image from 'next/image';
+import { useAuthState } from '../context/auth';
 
 const Home: NextPage = () => {
   const fetcher = async (url: string) => (await axios.get(url)).data;
 
+  const { authenticated } = useAuthState();
   const address = 'http://localhost:4000/api/subs/sub/topSubs';
 
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
-
-  console.log(topSubs);
 
   return (
     <div className='mx-auto flex max-w-5xl px-4 pt-5'>
@@ -24,15 +25,38 @@ const Home: NextPage = () => {
             <p className='text-center text-lg font-semibold'>상위 커뮤니티</p>
           </div>
           {/* 커뮤니티 리스트 */}
-          <div></div>
-          <div className='w-full py-6 text-center'>
-            <Link
-              href='/subs/create'
-              className='w-full rounded bg-gray-400 p-2 text-center text-white'
-            >
-              커뮤니티 만들기
-            </Link>
+          <div>
+            {topSubs?.map((sub) => (
+              <div
+                key={sub.name}
+                className='flex items-center border-b px-4 py-2 text-xs'
+              >
+                <Link href={`/r/${sub.name}`}>
+                  <Image
+                    src={sub.imageUrl}
+                    className='cursor-pointer rounded-full'
+                    alt='Sub'
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+                <Link href={`/r/${sub.name}`} className='font ml-2'>
+                  {sub.name}
+                </Link>
+                <p className='ml-auto font-medium'>{sub.postCount}</p>
+              </div>
+            ))}
           </div>
+          {authenticated && (
+            <div className='w-full py-6 text-center'>
+              <Link
+                href='/subs/create'
+                className='w-full rounded bg-gray-400 p-2 text-center text-white'
+              >
+                커뮤니티 만들기
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
