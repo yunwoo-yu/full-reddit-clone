44bca4e36116758c220c97f38361ce9ea45971eb
Ycow <skypnal12@gmail.com>
2023-06-01 22:34:11 +0900

    feat: comment ui 추가

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,18 +1,44 @@
-import { Post } from '@/src/types/types';
+import { useAuthState } from '@/src/context/auth';
+import { Comment, Post } from '@/src/types/types';
+import axios from 'axios';
 import dayjs from 'dayjs';
 import Link from 'next/link';
 
 import { useRouter } from 'next/router';
+import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
 
 const PostPage = () => {
   const router = useRouter();
+  const [newComment, setNewComment] = useState('');
+  const { authenticated, user } = useAuthState();
   const { identifier, sub, slug } = router.query;
   const { data: post, error } = useSWR<Post>(
     identifier && slug ? `/posts/${identifier}/${slug}` : null
   );
+  const { data: comments } = useSWR<Comment[]>(
+    identifier && slug ? `/posts/${identifier}/${slug}/comments` : null
+  );
 
-  console.log(post);
+  console.log(comments);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (newComment.trim() === '') {
+      return;
+    }
+
+    try {
+      await axios.post(`/posts/${post?.identifier}/${post?.slug}/comments`, {
+        body: newComment,
+      });
+
+      setNewComment('');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className='mx-auto flex max-w-5xl px-4 pt-5'>
@@ -48,6 +74,72 @@ const PostPage = () => {
                   </div>
                 </div>
               </div>
+              {/* 댓글 작성 구간 */}
+              <div className='mb-4  pr-6'>
+                {authenticated ? (
+                  <div>
+                    <p className='mb-1 text-xs'>
+                      <Link
+                        href={`/u/${user?.username}`}
+                        className='font-semibold text-blue-500'
+                      >
+                        {user?.username}
+                      </Link>{' '}
+                      으로 댓글 작성
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                      <textarea
+                        className='w-full rounded border border-gray-300 p-3 focus:border-gray-600 focus:outline-none'
+                        onChange={(e) => setNewComment(e.target.value)}
+                        value={newComment}
+                      ></textarea>
+                      <div className='flex justify-end'>
+                        <button
+                          className='rounded bg-gray-400 px-3 py-1 text-white'
+                          disabled={newComment.trim() === ''}
+                        >
+                          댓글 작성
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                ) : (
+                  <div className='flex items-center justify-between rounded border border-gray-200 px-2 py-4'>
+                    <p className='font-semibold text-gray-400'>
+                      댓글 작성을 위해서 로그인 해주세요.
+                    </p>
+                    <div>
+                      <Link
+                        href={`/login`}
+                        className='rounded bg-gray-400 px-3 py-1 text-white'
+                      >
+                        로그인
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
+              {/* 댓글 리스트 부분 */}
+              {comments?.map((comment) => (
+                <div className='flex' key={comment.identifier}>
+                  <div className='py-2 pr-2'>
+                    <p className='mb-1 text-xs leading-none'>
+                      <Link
+                        href={`/u/${comment.username}`}
+                        className='mr-1 font-bold hover:underline'
+                      >
+                        {comment.username}
+                      </Link>
+                      <span className='text-gray-600'>
+                        {`${comment.voteScore}
+                        posts
+                        ${dayjs(comment.createdAt).format('YYYY-MM-DD HH:mm')}
+                        `}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              ))}
             </>
           )}
         </div>
