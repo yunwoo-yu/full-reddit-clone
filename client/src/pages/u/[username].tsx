5547e902cbb82833d18395c60b8c4d9d22e16a93
Ycow <skypnal12@gmail.com>
2023-06-16 20:10:40 +0900

    feat: swr 데이터 로직 추가

--- client/src/pages/u/[username].tsx
+++ client/src/pages/u/[username].tsx
@@ -1,6 +1,14 @@
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 import React from 'react';
 
 const UserPage = () => {
+  const router = useRouter();
+  const username = router.query.username;
+
+  const { data, error } = useSWR(username ? `/users/${username}` : null);
+  if (!data) return null;
+
   return <div>UserPage</div>;
 };
 
