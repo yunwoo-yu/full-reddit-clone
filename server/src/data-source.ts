01c505cefa785ab3337e51a9a246da220fab617b
Ycow <skypnal12@gmail.com>
2023-06-19 14:14:49 +0900

    fix: data-source 호스트 변경

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,16 +1,16 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
+  type: "postgres",
+  host: "db",
   port: 5432,
-  username: 'postgres',
-  password: 'password',
-  database: 'postgres',
+  username: "postgres",
+  password: "password",
+  database: "postgres",
   synchronize: true,
   logging: false,
-  entities: ['src/entities/**/*.ts'],
+  entities: ["src/entities/**/*.ts"],
   migrations: [],
   subscribers: [],
 });
