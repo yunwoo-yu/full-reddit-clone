98f38199ab5d8971bb540e578fe550058f806190
Ycow <skypnal12@gmail.com>
2023-06-20 14:49:52 +0900

    fix: 주소 변경

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       'www.gravatar.com',
       'localhost',
-      'ec2-54-180-159-15.ap-northeast-2.compute.amazonaws.com',
+      'ec2-13-124-187-219.ap-northeast-2.compute.amazonaws.com',
     ],
   },
 };
