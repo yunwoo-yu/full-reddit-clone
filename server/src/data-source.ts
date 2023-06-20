495300b9c2aa2aea999127a3803562066782601a
Ycow <skypnal12@gmail.com>
2023-06-20 14:20:30 +0900

    fix: db 연동 에러 해결테스트

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "db",
+  host: "localhost",
   port: 5432,
   username: "postgres",
   password: "password",
